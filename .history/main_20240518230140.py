ff91bdfc917548e28d60ceb85c0e49de9ce1370b
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-05-26 09:56:03 +0700

    third commit

---
+++ .history/main_20240518230140.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+students = [
+    {'name': 'Student 1', 'age': 20},
+    {'name': 'Student 2', 'age': 18},
+    {'name': 'Student 3', 'age': 16}
+]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/students")
+def user_list():
+    return {'students': students}
+
+def student_check(student_id):
+    if not students
---
+++ .history/main_20240518235353.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from google.cloud import firestore
+
+db = firestore.Client()
+
+app = FastAPI()
+
+students = [
+    {'name': 'Student 1', 'age': 20},
+    {'name': 'Student 2', 'age': 18},
+    {'name': 'Student 3', 'age': 16}
+]
+
+class Student(BaseModel):
+    name: str
+    age: int
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/students")
+def user_list():
+    return {'students': students}
+    
+@app.get("/students/{student_id}")
+def user_detail(student_id: str):
+    doc_ref = db.collection('items').document(student_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        raise HTTPException(status_code= 404, detail="Student not found")
+
+@app.post('/students')
+async def user_add(student: Student):
+    student_data = student.model_dump()
+    doc_ref = db.collection('students').add(student_data)
+    return {"id": doc_ref[1].id, "data":student_data}
