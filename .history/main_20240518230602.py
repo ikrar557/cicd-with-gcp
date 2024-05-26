e13e67f597761f93962c9b4dfb900e2bedc6e325
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-05-26 09:51:03 +0700

    first commit

---
+++ .history/main_20240518230602.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = [
+    {'name': 'Student 1', 'age': 20},
+    {'name': 'Student 2', 'age': 18},
+    {'name': 'Student 3', 'age': 16}
+]
+
+class Student(BaseModel):
+    name
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/students")
+def user_list():
+    return {'students': students}
+
+def student_check(student_id):
+    if not students[student_id]:
+        raise HTTPException(status_code=404, detail='Student not found')
+    
+@app.get("/students/{student_id}")
+def user_detail(student_id: int):
+    student_check(student_id)
+    return {'student' : students[student_id]}
+
+@app.post('/students')
+def user_add(student: Student):
+    student.append(student)
+    
+    return {'student': student[-1]}
---
+++ .history/main_20240518235610.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from google.cloud import firestore
+
+db = firestore.Client()
+
+app = FastAPI()
+
+students = [
+    {'name': 'Student 1', 'age': 20},
+    {'name': 'Student 2', 'age': 18},
+    {'name': 'Student 3', 'age': 16}
+]
+
+class Student(BaseModel):
+    name: str
+    age: int
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/students")
+async def list_students():
+    try:
+        # Reference to the students collection
+        students_ref = db.collection('students')
+        # Fetch all documents in the students collection
+        docs = students_ref.stream()
+
+-        students = []
+
+        # Iterate over all documents and convert to dictionary
+        for doc in docs:
+            students.append(doc.to_dict())
+
+        return {'students': students}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/students/{student_id}")
+def user_detail(student_id: str):
+    doc_ref = db.collection('students').document(student_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        raise HTTPException(status_code= 404, detail="Student not found")
+
+@app.post('/students')
+async def user_add(student: Student):
+    student_data = student.model_dump()
+    doc_ref = db.collection('students').add(student_data)
+    return {"id": doc_ref[1].id, "data":student_data}
